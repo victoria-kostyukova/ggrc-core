362ead1d7a53bc072f498be78aefb745007e4cb5
khatkovski <pavelkhat@gmail.com>
2019-10-10 13:07:15 +0300

    Extend scoped tables with external attributes

--- test/integration/external_app/test_ext_scope_objects.py
+++ test/integration/external_app/test_ext_scope_objects.py
@@ -3,22 +3,26 @@
 
 """Tests for external ScopeObject models"""
 
+import ddt
 
 from ggrc import models
 from ggrc.models.mixins import synchronizable
 from integration.ggrc import TestCase
 
 
+@ddt.ddt
 class TestExternalScopeObject(TestCase):
-  """class for testing externalization for ScopeObject"""
+  """Class for testing externalization for ScopeObject"""
 
-  def setUp(self):
-    super(TestExternalScopeObject, self).setUp()
+  @ddt.data(*models.all_models.get_scope_models())
+  def test_scoped_are_synchronizable(self, model):
+    """Test that '{0.__name__}' model are Synchronizable"""
+    self.assertTrue(issubclass(model, synchronizable.Synchronizable))
+    self.assertTrue(hasattr(model, 'external_id'))
+    self.assertTrue(hasattr(model, 'external_slug'))
 
-  scoped_classes = models.all_models.get_scope_models()
-
-  def test_scoped_are_synchronizable(self):
-    """test that all scope objects  are Synchronizable"""
-    self.assertEqual(15, len(self.scoped_classes))
-    for model in self.scoped_classes:
-      self.assertTrue(issubclass(model, synchronizable.Synchronizable))
+  @ddt.data(*models.all_models.get_scope_models())
+  def test_scoped_have_external_field(self, model):
+    """Test that '{0.__name__}' table has external_id/slug columns"""
+    self.assertIn('external_id', model.__table__.columns._data)
+    self.assertIn('external_slug', model.__table__.columns._data)
