dec1a9216521babfdbbedf96e4abf35616791c57
Aliaksandr Trush <aliaksandrt@google.com>
2019-12-11 17:44:05 +0300

    return snapshot_counts related to user permissions

--- test/integration/ggrc/services/resources/test_audit.py
+++ test/integration/ggrc/services/resources/test_audit.py
@@ -8,6 +8,8 @@ import ddt
 
 from ggrc.models import all_models
 from integration.ggrc import api_helper
+from integration.ggrc_basic_permissions.models \
+    import factories as rbac_factories
 from integration.ggrc.models import factories
 from integration.ggrc.services import TestCase
 
@@ -19,6 +21,13 @@ class TestAuditResource(TestCase):
   def setUp(self):
     super(TestAuditResource, self).setUp()
     self.api = api_helper.Api()
+
+  def _get_snapshots_count(self, object_id):
+    """Get snapshots count for Audit with id {object_id}"""
+    response = self.api.client.get(
+        "/api/audits/{}/snapshot_counts".format(object_id),
+    )
+    return json.loads(response.data)
 
   def test_snapshot_counts_query(self):
     """Test snapshot_counts endpoint"""
@@ -33,35 +42,8 @@ class TestAuditResource(TestCase):
       )
       audit_2 = factories.AuditFactory()
 
-    with factories.single_commit():
-      revision = all_models.Revision.query.filter(
-          all_models.Revision.resource_type == "Audit",
-          all_models.Revision.resource_id == audit_1.id
-      ).first()
-      revision_2 = all_models.Revision.query.filter(
-          all_models.Revision.resource_type == "Audit",
-          all_models.Revision.resource_id == audit_2.id
-      ).first()
-      snapshot = factories.SnapshotFactory(
-          parent=audit_1,
-          child_type=control.type,
-          child_id=control.id,
-          revision=revision
-      )
-      factories.RelationshipFactory(
-          source=audit_1,
-          destination=snapshot,
-      )
-      snapshot2 = factories.SnapshotFactory(
-          parent=audit_2,
-          child_type=regulation.type,
-          child_id=regulation.id,
-          revision=revision_2
-      )
-      factories.RelationshipFactory(
-          source=audit_2,
-          destination=snapshot2,
-      )
+    self._create_snapshots(audit_1, [control])
+    self._create_snapshots(audit_2, [regulation])
 
     audits = [audit_1, audit_2]
     expected_snapshot_counts = {
@@ -70,8 +52,55 @@ class TestAuditResource(TestCase):
     }
 
     for audit in audits:
-      response = self.api.client.get(
-          "/api/audits/{}/snapshot_counts".format(audit.id),
-      )
-      snapshot_counts = json.loads(response.data)
+      snapshot_counts = self._get_snapshots_count(audit.id)
       self.assertEqual(snapshot_counts, expected_snapshot_counts[audit.id])
+
+  # pylint:disable=invalid-name
+  def test_snapshot_counts_query_no_perm(self):
+    """Test snapshot_counts endpoint counts only Snapshots that user has
+    access to"""
+
+    with factories.single_commit():
+      audit = factories.AuditFactory()
+      assmnt = factories.AssessmentFactory(audit=audit)
+      factories.RelationshipFactory(source=audit, destination=assmnt)
+      audit_id = audit.id
+
+      # Create Global Creator and assign as Assignees
+      user = factories.PersonFactory()
+      reader_role = all_models.Role.query.filter(
+          all_models.Role.name == "Creator").first()
+      rbac_factories.UserRoleFactory(role=reader_role, person=user)
+      acr = all_models.AccessControlRole.query.filter_by(
+          name="Assignees",
+          object_type=assmnt.type,
+      ).first()
+      factories.AccessControlPersonFactory(
+          person_id=user.id,
+          ac_list=assmnt.acr_acl_map[acr],
+      )
+
+      control_1 = factories.ControlFactory()
+      control_2 = factories.ControlFactory()
+      regulation = factories.RegulationFactory()
+      factories.RelationshipFactory(
+          source=audit,
+          destination=control_1
+      )
+
+    snapshots = self._create_snapshots(audit, [control_1,
+                                               control_2,
+                                               regulation])
+    factories.RelationshipFactory(
+        source=assmnt,
+        destination=snapshots[0]
+    )
+
+    self.api.set_user(user)
+
+    expected_snapshot_counts = {
+        audit_id: {"Control": 1},
+    }
+
+    snapshot_counts = self._get_snapshots_count(audit_id)
+    self.assertEqual(expected_snapshot_counts[audit_id], snapshot_counts)
