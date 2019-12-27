22b1fd128bb635a278f042dc88458f231a314b40
Rajesh Kottakota <rkottakota@google.com>
2019-12-27 18:08:56 +0800

    Rework test_audits.py

--- test/integration/ggrc/services/test_audits.py
+++ test/integration/ggrc/services/test_audits.py
@@ -3,10 +3,8 @@
 
 """Tests for actions available on audits handle."""
 
-from ggrc.models import Audit
-from ggrc.models import Program
-from ggrc.models import all_models
 
+from ggrc.models import all_models
 from integration.ggrc import generator
 from integration.ggrc import TestCase
 from integration.ggrc.query_helper import WithQueryApi
@@ -17,20 +15,24 @@ class TestAuditActions(TestCase, WithQueryApi):
   """Test Audit related actions"""
 
   def setUp(self):
-    self.clear_data()
     super(TestAuditActions, self).setUp()
     self.client.get("/login")
     self.gen = generator.ObjectGenerator()
-    filename = "program_audit.csv"
-    self.import_file(filename)
-    self.assertEqual(2, Audit.query.count())
-    audit = Audit.query.filter(Audit.slug == "Aud-1").first()
-    program = Program.query.filter(Program.slug == "prog-1").first()
-    self.assertEqual(audit.program_id, program.id)
 
   def test_filter_by_evidence_url(self):
     """Filter by = operator."""
-    evidence_url = "http://i.imgur.com/Lppr247.jpg"
+    with factories.single_commit():
+      audit = factories.AuditFactory()
+      evidence_url = "http://i.imgur.com/Lppr247.jpg"
+      evidence = factories.EvidenceFactory(
+          link=evidence_url,
+          kind=all_models.Evidence.URL,
+          parent_obj={
+              'id': audit.id,
+              'type': audit.type
+          }
+      )
+      factories.RelationshipFactory(source=audit, destination=evidence)
     audits = self._get_first_result_set(
         self._make_query_dict("Audit",
                               expression=["evidence url", "=", evidence_url]),
@@ -60,7 +62,7 @@ class TestAuditActions(TestCase, WithQueryApi):
     evidence = all_models.Evidence.query.get(evidence.id)
     self.assertEqual(evidence.link, "test_link")
 
-    audit = Audit.query.filter(Audit.slug == "Aud-1").first()
+    audit = factories.AuditFactory(title="Audit")
     data = {
         "source": self.gen.create_stub(audit),
         "destination": self.gen.create_stub(evidence),
