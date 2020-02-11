dd02bcb8f37a9bef4021fd6fe7e8948b6cdade55
mrR2D2 <alaksandr.lavoncyk@gmail.com>
2020-02-11 12:49:48 +0300

    Create mixin with default update_attributes impl

--- src/ggrc/migrations/markdown/base.py
+++ src/ggrc/migrations/markdown/base.py
@@ -3,6 +3,7 @@
 
 """Base module for convert attributes from html to markdown."""
 
+import collections
 import datetime
 
 import sqlalchemy as sa
@@ -178,3 +179,127 @@ class BaseConverter(object):
           attribute_value=self._parse_html(attribute_value),
           updated_at=datetime.datetime.utcnow(),
       ).where(cavs_table.c.id == cav_id))
+
+
+class WithDefaultUpdateAttrMixin(object):
+  """Mixin providing the default implementation of `update_attributes()`.
+
+  This mixin provides the default implementation of `update_attributes()`
+  method for any `BaseConverter` subclass.
+
+  The fields that should be sanitized from HTML should be provided via
+  `COLUMNS_TO_CONVERT` iterable class field.
+  """
+
+  COLUMNS_TO_CONVERT = []
+
+  @property
+  def select_statement(self):
+    """Return SQL query which selects data that should be updated.
+
+    Returns:
+      A string representing SQL query.
+    """
+    sql = """
+        SELECT id, {columns}
+          FROM {table_name}
+         WHERE {conditions}
+    """
+
+    columns_to_convert = sorted(self.COLUMNS_TO_CONVERT)
+
+    columns = ", ".join(columns_to_convert)
+    condition_template = "{column} REGEXP :reg_exp"
+    conditions = " OR ".join([
+        condition_template.format(column=column)
+        for column in columns_to_convert
+    ])
+
+    return sql.format(
+        columns=columns,
+        table_name=self.TABLE_NAME,
+        conditions=conditions,
+    )
+
+  @property
+  def database_table(self):
+    """Return representation of `TABLE_NAME` database table.
+
+    Returns:
+      A sqlalchemy.TableClause object representing table in the database.
+    """
+    return sa.sql.table(
+        self.TABLE_NAME,
+        sa.Column("id", sa.Integer()),
+        sa.Column("updated_at", sa.DateTime, nullable=False),
+        *[
+            sa.Column(column, sa.Text, nullable=False)
+            for column in self.COLUMNS_TO_CONVERT
+        ]
+    )
+
+  def update_attributes(self, connection):
+    """Update object attributes from HTML to markdown.
+
+    Args:
+      connection (sqlalchemy.Connection): A sqlalchemy connection object.
+
+    Returns:
+      A list of object IDs that were modified.
+    """
+    objects_data = self._get_objects_data(connection)
+    obj_ids = {obj[0] for obj in objects_data}
+    self._processing_attributes(objects_data)
+
+    return obj_ids
+
+  def _get_objects_data(self, connection):
+    """Get objects data.
+
+    Args:
+      connection (sqlalchemy.Connection): A sqlalchemy connection object.
+
+    Returns:
+      A list of object records.
+    """
+    return connection.execute(
+        sa.text(self.select_statement),
+        reg_exp=self.REGEX_HTML,
+    ).fetchall()
+
+  @classmethod
+  def _build_new_values(cls, obj):
+    """Build a dict with new values for the provided object.
+
+    Returns:
+      A dict where keys are column names, and values are column values
+        sanitized from HTML.
+    """
+    return {
+        field: cls._parse_html(getattr(obj, field))
+        for field in cls.COLUMNS_TO_CONVERT
+    }
+
+  def _processing_attributes(self, objects_data):
+    """Convert objects attributes from HTML to markdown.
+
+    Args:
+      objects_data (list): A list of object data records.
+    """
+    object_table = self.database_table
+
+    columns_to_convert = sorted(self.COLUMNS_TO_CONVERT)
+    data_structure_fields = ["c_id"] + columns_to_convert
+    data_structure = collections.namedtuple("Record", data_structure_fields)
+
+    for obj in objects_data:
+      obj = data_structure(*obj)
+      new_values = self._build_new_values(obj)
+      update_statement = object_table.update().values(
+          updated_at=datetime.datetime.utcnow(),
+          **new_values
+      ).where(
+          object_table.c.id == obj.c_id,
+      )
+
+      op.execute(update_statement)
