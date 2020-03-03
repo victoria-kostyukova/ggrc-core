fe37ae4b167cb825c291344713de4f3a0864d3b1
Shurke <alsereda1992@gmail.com>
2020-03-03 15:35:53 +0300

    Add filter for suggest emails

--- src/ggrc/services/suggest.py
+++ src/ggrc/services/suggest.py
@@ -10,6 +10,7 @@ from flask import request
 from sqlalchemy import or_
 from sqlalchemy.orm import load_only
 
+from ggrc import db
 from ggrc.integrations import client
 from ggrc import settings
 from ggrc.models import all_models
@@ -42,21 +43,54 @@ def suggest():
     return mock_suggest()
 
   tokens = request.args.get("prefix", "").split()
+  initial_email = request.args.get("initialEmail", None)
   if tokens:
     person_client = client.PersonClient()
     entries = person_client.suggest_persons(tokens)
-    return make_suggest_result(entries)
+    return make_suggest_result(entries, initial_email)
   return make_suggest_result([])
 
 
-def make_suggest_result(entries):
+def _filter_emails(raw_suggest, initial_email):
+  """Filter out persons existing in the DB from suggested people.
+
+  Args:
+    raw_suggest: a list of dicts with suggested by HR API people
+    initial_email: initial email address when editing user information
+  Returns:
+    A list of dicts representing persons who are not in DB.
+  """
+  existed_emails = db.session.query(all_models.Person.email).filter(
+      all_models.Person.email.in_(
+          [suggest_person["email"] for suggest_person in raw_suggest]
+      )
+  ).all()
+  existed_emails = {email for email, in existed_emails}
+  filtered_suggest = []
+  for suggest_person in raw_suggest:
+    if suggest_person["email"] not in existed_emails:
+      filtered_suggest.append(suggest_person)
+  if initial_email:
+    existed_person = all_models.Person.query.filter(
+        all_models.Person.email == initial_email
+    ).one()
+    filtered_suggest.append({
+        "name": existed_person.name,
+        "email": existed_person.email
+    })
+  return filtered_suggest
+
+
+def make_suggest_result(entries, initial_email=None):
   """Build suggest response"""
   domain = getattr(settings, "AUTHORIZED_DOMAIN", "")
+  raw_suggest = [{
+      "name": "%s %s" % (entry["firstName"], entry["lastName"]),
+      "email": "%s@%s" % (entry["username"], domain),
+  } for entry in entries]
+  filtered_suggest = _filter_emails(raw_suggest, initial_email)
   return current_app.make_response((
-      json.dumps([{
-          "name": "%s %s" % (entry["firstName"], entry["lastName"]),
-          "email": "%s@%s" % (entry["username"], domain),
-      } for entry in entries]),
+      json.dumps(filtered_suggest),
       200,
       [('Content-Type', 'application/json')],
   ))
--- test/integration/ggrc/services/test_suggest.py
+++ test/integration/ggrc/services/test_suggest.py
@@ -65,18 +65,104 @@ class TestSuggest(TestCase):
           payload={'tokens': expected, }
       )
 
-  @ddt.data(1, 2)
   @mock.patch('ggrc.settings.INTEGRATION_SERVICE_URL', new='mock')
   @mock.patch('ggrc.settings.AUTHORIZED_DOMAIN', new='example.com')
-  def test_mock_suggest_limit(self, limit):
-    """Test limit count of results returned by suggest."""
+  def test_suggest_for_exist(self):
+    """Test no suggest for exist person."""
     with factories.single_commit():
       factories.PersonFactory(email='qwerty1@example.com')
       factories.PersonFactory(email='qwerty2@example.com')
-    query = '/people/suggest?prefix={}&limit={}'.format('qw', limit)
+    query = '/people/suggest?prefix={}'.format('qw')
     response = self.api.client.get(query)
     self.assert200(response)
-    self.assertEqual(len(response.json), limit)
+    self.assertEqual(len(response.json), 0)
+
+  @mock.patch('ggrc.settings.INTEGRATION_SERVICE_URL', new='endpoint')
+  @mock.patch('ggrc.settings.AUTHORIZED_DOMAIN', new='example.com')
+  def test_suggest_for_creation(self):
+    """Test suggest in step creating new user."""
+    with factories.single_commit():
+      factories.PersonFactory(
+          email='john_b@example.com',
+          name='John Born'
+      )
+    query = '/people/suggest?prefix={}'.format('joh')
+    with mock.patch.multiple(
+        PersonClient,
+        _post=mock.MagicMock(return_value={'persons': [
+            {
+                'uri': '1912',
+                'username': 'john_b',
+                'personNumber': '1912',
+                'firstName': 'John',
+                'lastName': 'Born'
+            },
+            {
+                'uri': '1791',
+                'username': 'john_d',
+                'personNumber': '1791',
+                'firstName': 'John',
+                'lastName': 'Dory'
+            },
+        ]})
+    ):
+      expected_response = [
+          {'name': 'John Dory', 'email': 'john_d@example.com'},
+      ]
+      response = self.api.client.get(query)
+      self.assert200(response)
+      self.assertEqual(response.json, expected_response)
+
+  @mock.patch('ggrc.settings.INTEGRATION_SERVICE_URL', new='endpoint')
+  @mock.patch('ggrc.settings.AUTHORIZED_DOMAIN', new='example.com')
+  def test_suggest_for_editing(self,):
+    """Test suggest in step editing user."""
+    with factories.single_commit():
+      factories.PersonFactory(
+          email='john_b@example.com',
+          name='John Born'
+      )
+      factories.PersonFactory(
+          email='john_s@example.com',
+          name='John Smit'
+      )
+    query = '/people/suggest?prefix={}&initialEmail={}'.format(
+        'joh',
+        "john_s@example.com"
+    )
+    with mock.patch.multiple(
+        PersonClient,
+        _post=mock.MagicMock(return_value={'persons': [
+            {
+                'uri': '1912',
+                'username': 'john_b',
+                'personNumber': '1912',
+                'firstName': 'John',
+                'lastName': 'Born'
+            },
+            {
+                'uri': '1791',
+                'username': 'john_d',
+                'personNumber': '1791',
+                'firstName': 'John',
+                'lastName': 'Dory'
+            },
+            {
+                'uri': '666',
+                'username': 'john_s',
+                'personNumber': '666',
+                'firstName': 'John',
+                'lastName': 'Smit'
+            },
+        ]})
+    ):
+      expected_response = [
+          {'name': 'John Dory', 'email': 'john_d@example.com'},
+          {'name': 'John Smit', 'email': 'john_s@example.com'}
+      ]
+      response = self.api.client.get(query)
+      self.assert200(response)
+      self.assertEqual(response.json, expected_response)
 
   @ddt.data('', '  ')
   @mock.patch('ggrc.settings.INTEGRATION_SERVICE_URL', new='endpoint')
