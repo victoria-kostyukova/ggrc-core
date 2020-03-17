06c1b112ccdf37b1b8b7637927499e74c4aa5947
wofec <wofecchuan@gmail.com>
2020-03-17 18:36:49 +0300

    Add event handler for unarchive-link

--- src/ggrc-client/js/components/unarchive-link.js
+++ src/ggrc-client/js/components/unarchive-link.js
@@ -8,30 +8,29 @@ import canMap from 'can-map';
 import canComponent from 'can-component';
 export default canComponent.extend({
   tag: 'unarchive-link',
-  view: canStache('<a href="javascript:void(0)"><content></content></a>'),
+  view: canStache('<a on:el:click="unarchive(scope.event)" ' +
+    'href="javascript:void(0)"><content></content></a>'),
   leakScope: true,
   viewModel: canMap.extend({
     notify: '',
     instance: null,
     notifyText: 'was unarchived successfully',
-  }),
-  events: {
-    'a click': function (el, event) {
-      let instance = this.viewModel.attr('instance');
-      let notifyText = instance.display_name() + ' ' +
-        this.viewModel.attr('notifyText');
+    unarchive(event) {
+      const instance = this.attr('instance');
+      const notifyText = instance.display_name() + ' ' +
+        this.attr('notifyText');
 
       event.preventDefault();
 
       if (instance && instance.attr('archived')) {
         instance.attr('archived', false);
         instance.save()
-          .then(function () {
-            if (this.viewModel.attr('notify')) {
+          .then(() => {
+            if (this.attr('notify')) {
               $('body').trigger('ajax:flash', {success: notifyText});
             }
-          }.bind(this));
+          });
       }
     },
-  },
+  }),
 });
