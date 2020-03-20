7bb532e4bffa7922ad3642baa18608fee69b0c2d
wofec <wofecchuan@gmail.com>
2020-03-20 16:18:55 +0300

    Update unit tests for unarchive-link

--- src/ggrc-client/js/components/tests/unarchive-link_spec.js
+++ src/ggrc-client/js/components/tests/unarchive-link_spec.js
@@ -5,74 +5,61 @@
 
 import canMap from 'can-map';
 import Component from '../unarchive-link';
+import {getComponentVM} from '../../../js_specs/spec-helpers';
 
-describe('unarchive-link component', function () {
-  describe('click() event', function () {
-    let displayName = 'DISPLAY NAME';
-    let notifyText = 'was unarchived successfully';
-    let eventObj;
-    let instance;
-    let pendingSave;
-    let viewModel;
-    let $element;
-    let event;
+describe('unarchive-link component', () => {
+  let viewModel;
 
-    beforeEach(function () {
-      pendingSave = new $.Deferred();
+  beforeEach(() => {
+    viewModel = getComponentVM(Component);
+  });
 
-      instance = new canMap({
-        save: jasmine.createSpy('save')
-          .and.returnValue(pendingSave.promise()),
-        display_name: function () {
-          return displayName;
-        },
-      });
-
-      viewModel = new canMap({
-        instance: instance,
-        notifyText: notifyText,
-      });
-      eventObj = $.Event();
-      $element = $('<div></div>');
-
-      spyOn($.fn, 'trigger').and.callThrough();
-
-      event = Component.prototype.events['a click']
-        .bind({viewModel});
+  describe('unarchive() method', () => {
+    beforeEach(() => {
+      viewModel.attr('instance', new canMap({
+        display_name: () => 'fake_instance',
+        save: jasmine.createSpy(),
+      }));
     });
 
-    it('instance was not unarchived if was not archived', function () {
-      event($element, eventObj);
+    describe('if instance is archived', () => {
+      let event;
 
-      expect(instance.attr('archived')).toBeFalsy();
-      expect(instance.save).not.toHaveBeenCalled();
-      expect($.fn.trigger).not.toHaveBeenCalled();
-    });
-
-    it('instance was saved without notification', function (done) {
-      instance.attr('archived', true);
-
-      event($element, eventObj);
-      pendingSave.resolve().then(() => {
-        expect(instance.attr('archived')).toBeFalsy();
-        expect(instance.save).toHaveBeenCalled();
-        expect($.fn.trigger).not.toHaveBeenCalled();
-        done();
+      beforeEach(() => {
+        viewModel.attr('instance.archived', true);
+        event = {
+          preventDefault: jasmine.createSpy(),
+        };
       });
-    });
 
-    it('instance was saved without notification', function (done) {
-      let successMessage = displayName + ' ' + notifyText;
-      instance.attr('archived', true);
-      viewModel.attr('notify', true);
+      it('sets false to instance.archived attribute', () => {
+        viewModel.attr('instance.save')
+          .and.returnValue(new Promise(() => {}));
 
-      event($element, eventObj);
-      pendingSave.resolve().then(() => {
-        expect(instance.attr('archived')).toBeFalsy();
-        expect(instance.save).toHaveBeenCalled();
+        viewModel.unarchive(event);
+
+        expect(viewModel.attr('instance.archived')).toBe(false);
+      });
+
+      it('calls instance\'s save() method', () => {
+        viewModel.attr('instance.save')
+          .and.returnValue(new Promise(() => {}));
+
+        viewModel.unarchive(event);
+
+        expect(viewModel.attr('instance.save')).toHaveBeenCalled();
+      });
+
+      it('trigger ajax:flash after instance.save() success', async () => {
+        viewModel.attr('instance.save')
+          .and.returnValue(Promise.resolve());
+        viewModel.attr('notify', 'success');
+        spyOn($.fn, 'trigger').and.callThrough();
+
+        await viewModel.unarchive(event);
+
         expect($.fn.trigger).toHaveBeenCalledWith('ajax:flash',
-          {success: [successMessage]});
-        done();
+          {success: ['fake_instance was unarchived successfully']});
       });
     });
   });
